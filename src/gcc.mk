94b4fcc8e09a7a9662fae08c59b89aa3917ea8d1
Terry M. Poulin <BigBoss1964@gmail.com>
2016-08-31 23:14:25 +0000

    Move out common bits of gcc.mk to unix.mk.

--- src/gcc.mk
+++ src/gcc.mk
@@ -1,31 +1,5 @@
 
 CC = c99
 CFLAGS = -Wall
-VPATH = ./src
-O = ./obj
-OBJS = $(addprefix $O/, $(addsuffix .o, system main debug options io))
 
-.SUFFIXES:
-
-zar: $(OBJS)
-	$(LINK.c) $^ -o $@
-
-$(O)/%.o : %.c
-	$(COMPILE.c) $< -o $@
-
-
-clean:
-	rm -f $O/*.o
-	rm -f zar
-
-test:
-	rm -f wtf.zar
-	@echo Test...creating ZAR archive
-	./zar -c -f wtf.zar tmp/eggs tmp/ham tmp/quux tmp/spam
-	@echo Test...listing ZAR archive
-	./zar -t -f wtf.zar
-	@echo Test...extracting ZAR archive
-	rm -rf wtf.test
-	-mkdir wtf.test
-	-mkdir wtf.test\tmp
-	./zar -x -f wtf.zar -C wtf.test
+include src/unix.mk
---
+++ src/unix.mk
@@ -0,0 +1,32 @@
+#
+# $(toolchain).mk's targeting UNIX systems should include this file.
+#
+
+VPATH = ./src
+O = ./obj
+OBJS = $(addprefix $O/, $(addsuffix .o, system main debug options io))
+
+.SUFFIXES:
+
+zar: $(OBJS)
+	$(LINK.c) $^ -o $@
+
+$(O)/%.o : %.c
+	$(COMPILE.c) $< -o $@
+
+
+clean:
+	rm -f $O/*.o
+	rm -f zar
+
+test:
+	rm -f wtf.zar
+	@echo Test...creating ZAR archive
+	./zar -c -f wtf.zar tmp/eggs tmp/ham tmp/quux tmp/spam
+	@echo Test...listing ZAR archive
+	./zar -t -f wtf.zar
+	@echo Test...extracting ZAR archive
+	rm -rf wtf.test
+	-mkdir wtf.test
+	-mkdir wtf.test\tmp
+	./zar -x -f wtf.zar -C wtf.test
